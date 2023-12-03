d8bac57660354627c97091398d0f5dd911e60107
Dhananjay Thosar <dhananjaythosar2002@gmail.com>
2023-12-03 15:50:43 +0530

    Added functionality of calculator

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 console.log('Running...');
-let num1 = document.getElementById('num1-el');
-num1.innerText = 4
-console.log(num1);
+let input1 = document.getElementById('num1-el');
+let input2 = document.getElementById('num2-el');
+
+function num1() {
+	input1.innerText = prompt('Enter the 1st Number:');
+}
+
+function num2() {
+	input2.innerText = prompt('Enter the 2nd Number:');
+}
+
+function add() {
+	let result = Number(input1.innerText) + Number(input2.innerText);
+	document.getElementById('res-el').innerText = 'Result : ' + result;
+}
+
+function sub() {
+	let result = Number(input1.innerText) - Number(input2.innerText);
+	document.getElementById('res-el').innerText = 'Result : ' + result;
+}
+
+function mul() {
+	let result = Number(input1.innerText) * Number(input2.innerText);
+	document.getElementById('res-el').innerText = 'Result : ' + result;
+}
+function div() {
+	let result = Number(input1.innerText) / Number(input2.innerText);
+	document.getElementById('res-el').innerText = 'Result : ' + result;
+}
